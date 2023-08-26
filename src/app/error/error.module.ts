c2420faac224ba26ec3431583d4a2f2a0cfa5d0b
Samu <samu@zapasoft.com>
2023-08-26 19:16:03 +0200

    Translation module added to pages modules

--- src/app/error/error.module.ts
+++ src/app/error/error.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorRoutingModule } from './error-routing.module';
 import { PageNotFoundComponent } from './pages/pageNotFound/pageNotFound.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [PageNotFoundComponent],
-  imports: [CommonModule, ErrorRoutingModule],
+  imports: [CommonModule, ErrorRoutingModule, TranslateModule],
 })
 export class ErrorModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, TranslateModule],
 })
 export class HomeModule {}

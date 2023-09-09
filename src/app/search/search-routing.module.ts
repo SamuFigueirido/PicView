642bbfdc35bcac4ae3c3514d76dad71641d1b651
Samu <samu@zapasoft.com>
2023-09-09 21:27:26 +0200

    Removed unused imports

--- src/app/search/search-routing.module.ts
+++ src/app/search/search-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SearchComponent } from './pages/search/search.component';
 import { RouterModule, Routes } from '@angular/router';
 

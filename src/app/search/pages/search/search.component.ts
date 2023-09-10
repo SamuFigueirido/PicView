e94664b787e3f6f890fd4069382be0a624bf7100
Samu <samu@zapasoft.com>
2023-09-10 19:41:43 +0200

    Component searchbar added to Search Page with its event to set the query params

--- src/app/search/pages/search/search.component.ts
+++ src/app/search/pages/search/search.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
+  // Query
+  query: string = '';
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.activatedroute.queryParams.subscribe((params) => {
+      if (params['q']) {
+        this.query = params['q'];
+      }
+    });
+  }
 
+  /**
+   * Function to handle the search event of the searchbar
+   * @param query string with the query to search
+   */
+  getSearchQuery(query: string) {
+    this.router.navigate([], {
+      relativeTo: this.activatedroute,
+      queryParams: { q: query },
+      queryParamsHandling: 'merge',
+    });
+  }
 }

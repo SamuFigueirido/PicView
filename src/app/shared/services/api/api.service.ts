dd891a7ed62d8db6c60ef5ef4e52f3529822d771
Samu <samu@zapasoft.com>
2023-09-10 19:42:39 +0200

    Function to get an array of images by a query string

--- src/app/shared/services/api/api.service.ts
+++ src/app/shared/services/api/api.service.ts
@@ -45,4 +45,25 @@ export class ApiService {
         return data;
       });
   }
+
+  /**
+   * Function to get the images from the API by query
+   * @param query string with the query to search
+   * @param page number with the page to get the images
+   * @returns a promise with the images
+   */
+  getImagesByQuery(query: string, page: number) {
+    return fetch(
+      `${environment.api}search/photos?query=${query}&page=${page}&client_id=${environment.accessKey}`,
+      {
+        method: 'GET',
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      });
+  }
 }

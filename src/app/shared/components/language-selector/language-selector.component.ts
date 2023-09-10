0704bf73d8a2ac1f5938ab12b40e55434f3b7753
Samu <samu@zapasoft.com>
2023-09-10 22:02:31 +0200

    Component language selector to select the language

---
+++ src/app/shared/components/language-selector/language-selector.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from '../../services/language/language.service';
+
+@Component({
+  selector: 'language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrls: ['./language-selector.component.scss'],
+})
+export class LanguageSelectorComponent implements OnInit {
+  languages: any = [];
+
+  constructor(public languageSvc: LanguageService) {}
+
+  ngOnInit(): void {
+    this.languages = this.languageSvc.languages;
+  }
+
+  /**
+   * Function to set the language
+   * @param lang string with the language to set
+   */
+  setLanguage(lang: string) {
+    this.languageSvc.setLanguage(lang);
+  }
+}

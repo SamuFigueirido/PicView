6c60031881c87a5c206e08bedb6039d09e7a340c
Samu <samu@zapasoft.com>
2023-09-11 11:11:08 +0200

    Fixed deleted import unused

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,5 @@
-import { unsplash } from "./unsplash.environment";
-
 export const environment = {
   production: true,
   api: 'https://api.unsplash.com/',
   accessKey: process.env['UNSPLASH_ACCESS_KEY'],
-
-  /** Uncomment this line to import the unsplash environment */
-  // ...unsplash,
 };

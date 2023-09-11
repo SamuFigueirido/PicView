d8d49881af4d996b4f5a28c9c87483175b397042
Samu <samu@zapasoft.com>
2023-09-11 11:08:41 +0200

    Set environment unsplash key for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import { unsplash } from "./unsplash.environment";
 export const environment = {
   production: true,
   api: 'https://api.unsplash.com/',
-  accessKey: unsplash.accessKey,
+  accessKey: process.env['UNSPLASH_ACCESS_KEY'],
 
   /** Uncomment this line to import the unsplash environment */
   // ...unsplash,
